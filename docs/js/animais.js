47f43e78c37e558351ffc6750b19aca874d9a627
tousoaverfam <apenasaobservar@gmail.com>
2026-07-07 14:05:52 +0100

    Update animais.js

--- docs/js/animais.js
+++ docs/js/animais.js
@@ -1,1 +1,132 @@
+const animais = [
 
+    {
+        id: 1,
+
+        nome: "Luna",
+
+        especie: "gato",
+
+        tipo: "Gato",
+
+        idade: "2 anos",
+
+        sexo: "Fêmea",
+
+        descricao:
+            "A Luna é uma gata muito meiga e sociável. Adora companhia e momentos tranquilos.",
+
+
+        historia:
+            "Foi encontrada numa situação vulnerável e recuperou com muito carinho e dedicação. Agora procura uma família onde possa ser feliz.",
+
+
+        caracteristicas: [
+            "Esterilizada",
+            "Vacinada",
+            "Desparasitada"
+        ],
+
+
+        imagens: [
+
+            "assets/images/placeholder.jpg",
+
+            "assets/images/placeholder.jpg",
+
+            "assets/images/placeholder.jpg"
+
+        ]
+
+    },
+
+
+
+
+
+    {
+        id: 2,
+
+        nome: "Max",
+
+        especie: "cao",
+
+        tipo: "Cão",
+
+        idade: "4 anos",
+
+        sexo: "Macho",
+
+        descricao:
+            "O Max é um cão energético, brincalhão e muito leal.",
+
+
+        historia:
+            "Passou algum tempo à espera de uma oportunidade. É um animal muito equilibrado e preparado para integrar uma nova família.",
+
+
+        caracteristicas: [
+            "Vacinado",
+            "Microchipado",
+            "Sociável"
+        ],
+
+
+        imagens: [
+
+            "assets/images/placeholder.jpg",
+
+            "assets/images/placeholder.jpg",
+
+            "assets/images/placeholder.jpg"
+
+        ]
+
+    },
+
+
+
+
+
+
+    {
+        id: 3,
+
+        nome: "Mia",
+
+        especie: "gato",
+
+        tipo: "Gato",
+
+        idade: "6 meses",
+
+        sexo: "Fêmea",
+
+        descricao:
+            "A Mia é uma gatinha curiosa, divertida e cheia de energia.",
+
+
+        historia:
+            "É uma jovem gata que procura uma família responsável que acompanhe o seu crescimento.",
+
+
+        caracteristicas: [
+            "Vacinada",
+            "Desparasitada"
+        ],
+
+
+        imagens: [
+
+            "assets/images/placeholder.jpg",
+
+            "assets/images/placeholder.jpg",
+
+            "assets/images/placeholder.jpg"
+
+        ]
+
+    }
+
+
+];
